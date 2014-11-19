1843b14faca02f5d75ab36ad5d4afed3c26baced
bkis <boerge.kiss@gmail.com>
2014-11-19 11:23:57 +0100

    finished, fixed + output

--- SurmiranTSVConverter/src/de/bkiss/surmirantsv/Main.java
+++ SurmiranTSVConverter/src/de/bkiss/surmirantsv/Main.java
@@ -1,8 +1,24 @@
 package de.bkiss.surmirantsv;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
+		TSVMapper tsvm = new TSVMapper();
+		Map<String,String> fieldMap = new HashMap<String,String>();
+		
+		fieldMap.put("ID", "RecID");
+		fieldMap.put("DStichwortSort", "DStichwort_sort");
+		fieldMap.put("RStichwortSort", "RStichwort_sort");
+		fieldMap.put("DSempraez", "DSubsemantik");
+		fieldMap.put("RSempraez", "RSubsemantik");
+		
+		List<String[]> tsv = tsvm.createTSV("input.tab", "surmiran.tab", fieldMap);
+		
+		tsvm.writeSV(tsv, "\t", "output.tab");
 		
 	}
 
--- SurmiranTSVConverter/src/de/bkiss/surmirantsv/TSVMapper.java
+++ SurmiranTSVConverter/src/de/bkiss/surmirantsv/TSVMapper.java
@@ -1,44 +1,90 @@
 package de.bkiss.surmirantsv;
 
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import com.opencsv.CSVReader;
+import java.util.Scanner;
+
 
 public class TSVMapper {
 	
 	
 	public List<String[]> createTSV(String tsvFieldNamesTargetPath, String tsvDataSrcPath, Map<String, String> mapSrcToTargetFieldNames){
-		List<String[]> srcData = readTSV(tsvDataSrcPath);
-		String[] targetFields = readTSV(tsvFieldNamesTargetPath).get(0);
+		List<String[]> srcData = readSV(tsvDataSrcPath, "\t");
+		String[] targetFields = readSV(tsvFieldNamesTargetPath, "\t").get(0);
 		
+		System.out.print("[INFO] applying mapped target field names: ");
+		//change mapped src field names to target field names
+		for (int i = 0; i < srcData.get(0).length ; i++){
+			System.out.print(srcData.get(0)[i] + "->");
+			if (mapSrcToTargetFieldNames.containsKey(srcData.get(0)[i]))
+				srcData.get(0)[i] = mapSrcToTargetFieldNames.get(srcData.get(0)[i]);
+			System.out.print(srcData.get(0)[i] + " ");
+		}
+		System.out.println("...OK");
 		
+		System.out.print("[INFO] adding unused target field names... ");
+		//add unused target field names
+		List<String> fields = new ArrayList<String>();
+		for (String s :srcData.get(0))
+			fields.add(s);
+		for (String s : targetFields)
+			if (!fields.contains(s))
+				fields.add(s);
+		srcData.remove(0);
+		String[] newFields = new String[fields.size()];
+		srcData.add(0, fields.toArray(newFields));
 		
-		return null;
+		System.out.println("OK");
+		return srcData;
 	}
 	
 	
-	private List<String[]> readTSV(String path){
-		FileReader fr;
-		CSVReader csvr;
-		List<String[]> srcData = null;
+	public void writeSV(List<String[]> svData, String delimiter, String path){
+		System.out.print("[INFO] writing data to " + path + " ... ");
+		StringBuilder sb = new StringBuilder();
+		
+		for (String[] sArr : svData){
+			for (String s : sArr){
+				sb.append(s + delimiter);
+			}
+			sb.append("\n");
+		}
 		
 		try {
-			fr = new FileReader(new File(path));
-			csvr = new CSVReader(fr, '\t');
-			srcData = csvr.readAll();
-			csvr.close();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream(path), "UTF-8"));
+			out.write(sb.toString());
+			out.flush();
+			out.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("OK");
+	}
+
+	
+	private List<String[]> readSV(String tsvPath, String delimiter){
+		System.out.print("[INFO] reading " + tsvPath + "... ");
+		List<String[]> tsv = new ArrayList<String[]>();
+		
+		try {
+			Scanner scanner = new Scanner(new File(tsvPath));
+			while (scanner.hasNextLine())
+				tsv.add(scanner.nextLine().split(delimiter));
+			scanner.close();
+		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
 		
-		return srcData;
+		System.out.println("OK");
+		return tsv;
 	}
 	
 }
